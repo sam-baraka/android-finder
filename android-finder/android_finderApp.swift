03bd68656d46041ca291db8adf9937c4cbe23b4f
Samuel Baraka <sababuvercetti@gmail.com>
2023-08-21 13:03:21 +0300

    Initial Commit

---
+++ android-finder/android_finderApp.swift
@@ -0,0 +1,20 @@
+//
+//  android_finderApp.swift
+//  android-finder
+//
+//  Created by Samuel Baraka on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct android_finderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
